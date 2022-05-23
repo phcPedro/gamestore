8407939a2b90f3eb9f3ccb8073bdca03547db18f
phcpedro <pedroohenrique.pedro@gmail.com>
2022-05-23 10:01:59 -0300

    feat(user):Add Swagger, ValidationPipe

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Length } from "class-validator";
+
 export class MakeUserDto{
+@Length(2,15)
+@ApiProperty({
+  description: "O nome do usuario pode conter no minimo 3 caracteres e no maximo 15.",
+  example: "Ed ,Jordanchristoph"
+})
+
 name: string
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
+import {ApiTags} from '@nestjs/swagger';
 import {MakeUserDto } from "./dto/create-user-dto";
 import { UserService } from "./user.service";
 
+@ApiTags('user')
 @Controller('user')
 
 export class UserController {
