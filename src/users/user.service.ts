0043570550c710948d2fcca530ad96ae745d7d9c
phcpedro <pedroohenrique.pedro@gmail.com>
2022-05-23 08:05:50 -0300

    feat(user): implement create users endpoint

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { MakeUserDto } from "./dto/create-user-dto";
+import { User } from "./entities/user.entity";
 
 @Injectable()
 
 export class UserService{
+  users: User[] = [];
+
   create(createUserDto: MakeUserDto) {
-    return`New user has been created Successfully!` + JSON.stringify(createUserDto);
+    const user : User = {id: this.users.length+1, ...createUserDto };
+
+    this.users.push(user);
+    return user;
   }
   findAll() {
     return"FindAll";
