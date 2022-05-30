d418d82963733b5d34a2cf8b60825effd5579e2c
phcpedro <pedroohenrique.pedro@gmail.com>
2022-05-30 20:33:51 -0300

    fix, prisma error

--- src/genders/genders.module.ts
+++ src/genders/genders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GendersService } from './genders.service';
 import { GendersController } from './genders.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GendersController],
   providers: [GendersService]
 })
