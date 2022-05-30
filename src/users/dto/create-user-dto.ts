080d463531111719903588b049df393bb7acf9a1
phcpedro <pedroohenrique.pedro@gmail.com>
2022-05-30 19:11:50 -0300

    feat(db): add gamesdb and geners model

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,27 +1,38 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length, Matches, MinLength } from "class-validator";
 
 
 export class MakeUserDto{
 
 id?: string;
+@Length(3,15)
+@IsString()
 @ApiProperty({
   description: "O nome do usuario pode conter no minimo 3 caracteres e no maximo 15.",
   example: "Jordanchristoph"
 })
 name: string;
+@Length(3,15)
+@IsString()
 @ApiProperty({
   description: "O nickname do usuario pode conter no minimo 3 caracteres e no maximo 15.",
   example: "JordanC92"
 })
 nickname: string;
+@IsEmail()
 @ApiProperty({
   description: "coloque seu email completo",
   example: "teste@gmail.com"
 })
 email: string;
+@MinLength(8)
+@IsString()
+@Matches(/((?=.*\W+))(?![.\n])(?=.*[A-Z]).*$/,{
+  message:'Tente uma mistura de caracteres especiais, letras maiusculas e numero para uma senha forte.'
+})
 @ApiProperty({
-  description: "Senha",
-  example: "123"
+  description: "Senha do usuario para afetur o login.",
+  example: "123@Teste."
 })
 password: string;
 @ApiProperty({
