a8ac10ff74ed7cff8d26a893d513a410defe8826
phcpedro <pedroohenrique.pedro@gmail.com>
2022-05-30 17:32:08 -0300

    feat(user):Implement delete

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,49 +1,63 @@
-import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
-import {ApiOperation, ApiTags} from '@nestjs/swagger';
-import {MakeUserDto } from "./dto/create-user-dto";
-import { UpdateUserDto } from "./dto/update-user-dto";
-import { User } from "./entities/user.entity";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { MakeUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
+import { User } from './entities/user.entity';
 
-import { UserService } from "./user.service";
+import { UserService } from './user.service';
 
 @ApiTags('user')
 @Controller('user')
-
 export class UserController {
-  constructor(private readonly userService: UserService){}
+  constructor(private readonly userService: UserService) {}
 
   @Get()
   @ApiOperation({
-    summary: 'Listar todos os usuarios.'
+    summary: 'Listar todos os usuarios.',
   })
-  findAll(): Promise<User[]>{
+  findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
   @ApiOperation({
-    summary: 'Procurar um usuario cadastrado pelo ID.'
+    summary: 'Procurar um usuario cadastrado pelo ID.',
   })
-  findOne(@Param('id')id: string): Promise<User>{
+  findOne(@Param('id') id: string): Promise<User> {
     return this.userService.findOne(id);
-
   }
 
   @Post()
   @ApiOperation({
-    summary: 'Cadastrar um novo usuario.'
+    summary: 'Cadastrar um novo usuario.',
   })
-  create(@Body()dto: MakeUserDto): Promise<User>{
+  create(@Body() dto: MakeUserDto): Promise<User> {
     return this.userService.create(dto);
-
   }
   @Patch(':id')
   @ApiOperation({
-    summary: 'Editar um usuario já cadastrado.'
+    summary: 'Editar um usuario já cadastrado.',
   })
-  update(@Param('id')id: string, @Body()dto: UpdateUserDto): Promise<User>{
-    return this.userService.update(id,dto);
-
+  update(@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<User> {
+    return this.userService.update(id, dto);
   }
 
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Excluir um usuario.',
+  })
+  delete(@Param('id') id: string) {
+    this.userService.delete(id);
+  }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,37 +1,38 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { MakeUserDto } from "./dto/create-user-dto";
-import { UpdateUserDto } from "./dto/update-user-dto";
-import { User } from "./entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { MakeUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-
-export class UserService{
-
-
-  constructor(private readonly prisma: PrismaService){}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
 
   create(dto: MakeUserDto): Promise<User> {
-    const data: User = {...dto};
-    return this.prisma.userdb.create({data});
+    const data: User = { ...dto };
+    return this.prisma.userdb.create({ data });
   }
 
-  findOne(id: string): Promise<User>{
-   return this.prisma.userdb.findUnique({
-     where: {id,}
-   })
+  findOne(id: string): Promise<User> {
+    return this.prisma.userdb.findUnique({
+      where: { id },
+    });
   }
 
-  findAll(): Promise<User[]>{
+  findAll(): Promise<User[]> {
     return this.prisma.userdb.findMany();
   }
 
   update(id: string, dto: UpdateUserDto): Promise<User> {
-    const data: Partial<User> = {...dto};
+    const data: Partial<User> = { ...dto };
 
-   return this.prisma.userdb.update({
-       where : { id },
-       data,
-     });
+    return this.prisma.userdb.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    await this.prisma.userdb.delete({where: {id}});
   }
 }
